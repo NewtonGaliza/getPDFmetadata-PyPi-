c79c4e550365129a5e072934924b431477202828
newtongaliza <newtonjgaliza@gmail.com>
2018-11-21 18:44:35 -0300

    change version number in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
   name = 'getPDFmetadata',
-  version = '1.0.2',
+  version = '1.0.3',
   author = 'Newton Galiza',
   author_email = 'newtonjgaliza@gmail.com',
   long_description = long_description,
