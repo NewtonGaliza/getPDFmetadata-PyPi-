6a36a2b03302877b13d30f4b1765550902c9b9a0
newtongaliza <newtonjgaliza@gmail.com>
2018-11-21 18:43:19 -0300

    change version of pypdf in setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setuptools.setup(
   url = 'https://github.com/NewtonGaliza/getPDFmetadata',
   packages = setuptools.find_packages(),
   classifiers = ['Programming Language :: Python :: 3'],
-  install_requires = ['PyPDF2']
+  install_requires = ['pypdf4']
 
 )
